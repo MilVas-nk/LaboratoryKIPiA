250bf3007b71acddba8bf5d6ad45bf9805de1e7d
MilVas <milvas.nk@gmail.com>
2025-11-05 14:11:42 +0300

    Version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "ru.milvas.lkipia"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
